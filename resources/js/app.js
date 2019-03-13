334af1b990716dfa2844c4649ac422ccfa574b5e
Funnyjoe <38206938+Funnyjoe@users.noreply.github.com>
2019-03-13 20:50:48 +0800

    add modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,7 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('evaluation', require('./components/Evaluation.vue').default);
+Vue.component('modal', require('./components/Modal.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -37,4 +38,6 @@ let token = localStorage.getItem("csrf");
                 'X-CSRF-TOKEN': token,
                 'X-Requested-With': 'XMLHttpRequest',
             };
+
+
 
--- resources/js/components/Evaluation.vue
+++ resources/js/components/Evaluation.vue
@@ -1,23 +1,87 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Evaluation Component</div>
-
-                    <div class="card-body">
-                        I'm an Evaluation component.
-                    </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">评估 |  <button id="show-modal" @click="showModal">Show Modal</button></div>
+          <div class="card-body">
+                <!-- use the modal component, pass in the prop -->
+            <modal v-if="isModalVisible" @close="isModalVisible = false">
+                <!--
+                you can use custom content here to overwrite
+                default content
+                -->
+                <h3 slot="header">查询</h3>
+                <div slot="body" class="form-group">
+                <label for="InputDate">选择日期</label>
+                <div class="input-group mb-3">
+                    <input
+                  type="date"
+                  class="form-control"
+                  id="InputDate"
+                >
+                
+                <div class="input-group-append">
+                    <label class="input-group-text" for="inputGroupSelect02">必选</label>
                 </div>
-            </div>
+                </div>
+                </div>
+
+            </modal>
+           
+            <form>
+              <div class="form-group">
+
+                <label for="inputGroupSelect02">选择评估类型</label>
+                <div class="input-group mb-3">
+                <select class="custom-select" id="inputGroupSelect02">
+                    <option selected>选择类型</option>
+                    <option value="1">面试</option>
+                    <option value="2">面试评估</option>
+                    <option value="3">普通评估</option>
+                    <div role="separator" class="dropdown-divider"></div>
+                    <option class="dropdown-item" href="#">其它</option>
+                </select>
+                <div class="input-group-append">
+                    <label class="input-group-text" for="inputGroupSelect02">必选</label>
+                </div>
+                </div>
+                <div class="form-group mb-3">
+                <label for="InputNote">备注</label>
+                <textarea
+                  type="text"
+                  class="form-control"
+                  id="InputNote"
+                ></textarea>
+              </div>
+              </div>
+              <button type="submit" class="btn btn-primary"><i class="fas fa-check-circle "></i><span></span>提交</button>
+            </form>
+            
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.');
-        }
-    }
+export default {
+  mounted() {
+    console.log(this.isModalVisible);
+  },
+  data () {
+      return {
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      showModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    },
+};
 </script>
